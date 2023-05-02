4bb3f63125b5c5f1df704b29ffc3916d5e4b856c
wendy <wendyz34@nycstudents.net>
2023-05-02 17:44:34 -0400

    print

--- src/TipCalc.java
+++ src/TipCalc.java
@@ -15,34 +15,12 @@ public class TipCalc extends JFrame implements ActionListener, KeyListener{
     private JTextField textField2;
     private JTextArea Total;
     private JTextArea textArea1;
+    private TipCalculator calc;
     private int percent = 0;
 
     public TipCalc(){
         createUIComponents();
     }
-    public void actionPerformed(ActionEvent e){
-        Object source = e.getSource();
-        if(source instanceof JButton){
-            JButton button = (JButton) source;
-            String text = button.getText();
-
-
-        }
-
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e){}
-    @Override
-    public void keyPressed(KeyEvent e){
-
-    }
-    @Override
-    public void keyReleased(KeyEvent e){}
-
-
-
-
     public void createUIComponents(){
         setContentPane(text);
         setTitle("MY GUI!");
@@ -50,36 +28,45 @@ public class TipCalc extends JFrame implements ActionListener, KeyListener{
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         button1.addActionListener(this);
         button2.addActionListener(this);
+        button3.addActionListener(this);
+        button4.addActionListener(this);
         textField1.addKeyListener(this);
         setVisible(true);
 
-        text = new JPanel();
-        textField1 = new JTextField();
-        System.out.println(textField1.getText());
 
-        text.add(textField1);
-        textField2 = new JTextField();
-        text.add(textField2);
-        Tip = new JTextArea();
-        text.add(Tip);
-        button1 = new JButton("Add");
-        text.add(button1);
-        button2 = new JButton("Add");
-        text.add(button2);
-        button3 = new JButton("Minus");
-        text.add(button3);
-        button4 = new JButton("Minus");
-        text.add(button4);
-        setVisible(true);
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
 
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if (source instanceof JButton) {
+            if (source instanceof JButton) {
+                JButton button = (JButton) source;
+                String text = button.getText();
+                if (button.equals(button1)) {
+                    double value = Double.parseDouble(textField1.getText());
+                    value += 0.5;
+                    textField1.setText(value + "");
+                    percent = Integer.parseInt(textField1.getText());
 
 
+                }
+            }
+
+
+        }
 
 
 
     }
-
-
 }
 
